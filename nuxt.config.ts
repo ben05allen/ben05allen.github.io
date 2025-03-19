9d99eb6416a19f34a972ed29792afe5c513f781a
ben05allen <ben05allen@hotmail.com>
2025-03-19 22:48:49 +0900

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  ssr: true,
+  app: {
+    head: {
+      title: "Ben's blog",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [{ rel: "icon", type: "image/svg", href: "/favicon.svg" }],
+    },
+  },
+  modules: [],
+});
